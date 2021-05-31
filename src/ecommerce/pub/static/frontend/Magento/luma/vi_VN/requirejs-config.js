af0210bacaef5381cd5fcd39edabb8d13ae99b9e
Your Name <you@example.com>
2021-05-31 16:48:56 +0000

    Add currency format

--- src/ecommerce/pub/static/frontend/Magento/luma/vi_VN/requirejs-config.js
+++ src/ecommerce/pub/static/frontend/Magento/luma/vi_VN/requirejs-config.js
@@ -1158,6 +1158,38 @@ require.config(config);
  *
  * NOTICE OF LICENSE
  *
+ * This source file is subject to the mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_CurrencyFormatter
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+var config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/view/summary/abstract-total': {
+                'Mageplaza_CurrencyFormatter/js/view/summary/abstract-total': true
+            }
+        }
+    }
+};
+
+require.config(config);
+})();
+(function() {
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
  * This source file is subject to the Mageplaza.com license that is
  * available through the world-wide-web at this URL:
  * https://www.mageplaza.com/LICENSE.txt
